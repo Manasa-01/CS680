b39a6e2c5992e404ff93788821b8c1f6865e1e62
Manasa-01 <55657576+Manasa-01@users.noreply.github.com>
2022-03-20 19:31:51 -0400

    Add files via upload

---
+++ HW 2/test/src/edu/umb/cs680/hw02/SingletonTest.java
@@ -0,0 +1,20 @@
+package edu.umb.cs680.hw02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SingletonTest {
+
+	@Test
+	public void getInstanceReturnNonNullValue() {
+		assertNotNull(Singleton.getInstance());
+	}
+	
+	@Test
+	public void getInstanceReturnIdenticalInstance() {
+		Singleton s1 = Singleton.getInstance();
+		Singleton s2 = Singleton.getInstance();
+		assertSame(s1, s2);
+	}
+}
